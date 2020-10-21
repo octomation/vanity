9d89d871b3698e0d635f9258c3a3227526c1877e
Kamil Samigullin <kamil@samigullin.info>
2020-10-21 21:30:39 +0000

    issue #6: remove go code from project

--- internal/cnf/features.go
+++
@@ -1,6 +0,0 @@
-package cnf
-
-import "go.octolab.org/toolkit/config"
-
-// Features defines a list of available features.
-var Features = config.Features{}
--- vendor/go.octolab.org/toolkit/config/go.mod
+++
@@ -1,8 +0,0 @@
-module go.octolab.org/toolkit/config
-
-go 1.11
-
-require (
-	github.com/stretchr/testify v1.6.1
-	gopkg.in/yaml.v2 v2.3.0
-)
