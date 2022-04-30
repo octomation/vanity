01d9bafe1d8d815f3e8bc34983320662cbb173ca
Kamil Samigullin <kamil@samigullin.info>
2022-04-30 09:34:12 +0300

    chore: remove shebang from non-executable bash scripts

--- bin/lib/core/compile.bash
+++ bin/lib/core/compile.bash
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # shellcheck source=modules.bash # modules
 
 compile() {
--- bin/lib/core/modules.bash
+++ bin/lib/core/modules.bash
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 modules() {
   local module name path git url
   for module in $(@modules); do
